ea5c702563c14dbb4b88f6534bdf4b6d4cf0365b
OddM91 <OddM64@gmail.com>
2023-02-23 14:40:40 +0100

    feat: Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
+import { LoginComponent } from './components/login/login.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
+import { TrainerComponent } from './components/trainer/trainer.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPage,
     CataloguePage,
-    TrainerPage
+    TrainerPage,
+    LoginComponent,
+    CatalogueComponent,
+    TrainerComponent,
+    PokemonListComponent,
+    PokemonListItemComponent
   ],
   imports: [
     BrowserModule,
