f9f084289124c806e90dedc4ceb87c6134a91f75
Luisa <luisa.mohle@no.experis.com>
2023-02-24 14:02:54 +0100

    feat: add cru(d) service methods for trainers

--- src/app/services/trainer.service.ts
+++ src/app/services/trainer.service.ts
@@ -9,10 +9,43 @@ export class TrainerService {
     'https://pickle-abounding-oriole.glitch.me/trainers';
   private readonly apiKey: string =
     'XJj0Tk5lqqjcbhYCZfSAocbwVD4n3nM6J19BRjAZzBCmM7G245K2AzfJTjd2FmOR';
+  private readonly protectedHeaders = {
+    'X-API-Key': this.apiKey,
+    'Content-Type': 'application/json',
+  };
 
   constructor(private readonly http: HttpClient) {}
 
   getTrainers(): Observable<Trainer[]> {
     return this.http.get<Trainer[]>(this.apiUrl);
+  }
+
+  getTrainer(username: string): Observable<Trainer> {
+    const requestUri = new URL(
+      '?' + new URLSearchParams({ username: username }),
+      this.apiUrl
+    ).toString();
+    return this.http.get<Trainer>(requestUri);
+  }
+
+  createTrainer(username: string): Observable<Trainer> {
+    const requestUri = new URL(this.apiUrl).toString();
+    const requestBody = JSON.stringify({ username: username, pokemon: [] });
+    const requestOptions = {
+      method: 'POST',
+      headers: this.protectedHeaders,
+    };
+
+    return this.http.post<Trainer>(requestUri, requestBody, requestOptions);
+  }
+
+  updateTrainer(userId: number, pokemon: string[]): Observable<Trainer> {
+    const requestUri = new URL(`${this.apiUrl}/${userId}`).toString();
+    const requestBody = JSON.stringify({ pokemon: pokemon });
+    const requestOptions = {
+      method: 'PATCH',
+      headers: this.protectedHeaders,
+    };
+    return this.http.patch<Trainer>(requestUri, requestBody, requestOptions);
   }
 }
