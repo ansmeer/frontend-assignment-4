eaac1f34688b5f8ccd7c88efd3e84a6e705f8267
Luisa <luisa.mohle@no.experis.com>
2023-02-27 15:51:47 +0100

    feat: add custom decorator @required
    App throws an error if component is rendered without required properties.
    This makes use of ! assertion safer.

--- src/app/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon';
 import { AuthService } from 'src/app/services/auth.service';
+import Required from 'src/app/utils/required';
 
 @Component({
   selector: 'app-pokemon-list-item',
@@ -8,16 +9,19 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./pokemon-list-item.component.css'],
 })
 export class PokemonListItemComponent implements OnInit {
-  @Input() data: Pokemon | undefined;
+  @Input('data')
+  @Required
+  pokemon!: Pokemon;
+
   captured = false;
 
   constructor(private readonly authService: AuthService) {}
 
   ngOnInit(): void {
-    if (this.data && this.authService.user) {
+    if (this.pokemon && this.authService.user) {
       if (
         this.authService.user.pokemon.filter(
-          (value) => value.name === this.data!.name
+          (value) => value.name === this.pokemon!.name
         ).length > 0
       ) {
         this.captured = true;
