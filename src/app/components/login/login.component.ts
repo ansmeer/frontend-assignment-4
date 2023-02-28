46b06c519739516d67920296b1e8f92adecddec4
Luisa <luisa.mohle@no.experis.com>
2023-02-28 13:30:54 +0100

    fix: forward from login if already logged in

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,11 +8,17 @@ import { LoginFormData } from '../login-form/login-form.component';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router
   ) {}
+
+  ngOnInit(): void {
+    if (this.authService.user) {
+      this.router.navigate(['catalogue']);
+    }
+  }
 
   handleLoginFormSubmit(formData: LoginFormData) {
     this.authService.login(formData.username);
