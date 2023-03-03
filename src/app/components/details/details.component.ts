a9a4395b3ad79ce0b3a39c615d375703bd85d006
Luisa <luisa.mohle@no.experis.com>
2023-03-03 16:54:42 +0100

    refactor: clean up properties and order

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -10,25 +10,22 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
-  pokemonId: string = '';
+  private _pokemonId = '';
+  public pokemon?: PokemonDetails;
+
   constructor(
     private readonly route: ActivatedRoute,
     private readonly pokemonService: PokemonService
   ) {}
 
-  pokemon?: PokemonDetails;
-
-  baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/';
-
   ngOnInit(): void {
-    this.pokemonId = String(this.route.snapshot.paramMap.get('pokemonId'));
-    this.pokemonService.getDetailedPokemon(this.pokemonId).subscribe({
+    this._pokemonId = String(this.route.snapshot.paramMap.get('pokemonId'));
+    this.pokemonService.getDetailedPokemon(this._pokemonId).subscribe({
       next: (pkmn) => {
         this.pokemon = pkmn;
         console.log(
           this.pokemon.sprites.other['official-artwork'].front_default
         );
-        //console.log(this.pokemon.sprites.other.official_artwork.front_default);
       },
       error: (error: HttpErrorResponse) => {
         console.log('Could not fetch pokemon.', error.message);
